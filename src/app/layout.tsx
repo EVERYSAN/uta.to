d0201db124ad89e40c26f9c9c456de4274441048
EVERYSAN <128579465+EVERYSAN@users.noreply.github.com>
2025-08-29 21:20:52 +0900

    Update layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { ReactNode } from 'react';
+
+export const metadata: Metadata = {
+  title: 'UtaMita Search',
+  description: '歌ってみた検索サイト (MVP)',
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
