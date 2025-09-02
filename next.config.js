2c397372a11407e2ba4f5f2ec00d51863cdc8123
EVERYSAN <128579465+EVERYSAN@users.noreply.github.com>
2025-09-02 17:44:52 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // ← ここに output: 'export' や basePath 等は入れない
+  async headers() {
+    const securityHeaders = [
+      {
+        key: 'Content-Security-Policy',
+        value: [
+          "default-src 'self';",
+          "img-src 'self' https://i.ytimg.com data: blob:;",
+          "media-src 'self' blob:;",
+          "frame-src https://www.youtube.com https://www.youtube-nocookie.com;",
+          "connect-src 'self' https://www.youtube.com https://www.youtube-nocookie.com https://i.ytimg.com https://s.ytimg.com https://www.google.com;",
+          "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://s.ytimg.com;",
+          "style-src 'self' 'unsafe-inline';",
+        ].join(' ')
+      },
+      { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' }
+    ];
+    return [{ source: '/(.*)', headers: securityHeaders }];
+  }
 };
-module.exports = nextConfig;
 
+module.exports = nextConfig;
